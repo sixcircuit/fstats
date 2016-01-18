aab1a976819e4df1b6c22d8f26cd8891ded25f26
Kendrick Taylor <kendrick@circuitsix.com>
2016-01-17 17:52:16 -0800

    add all time max to summary

--- test.sh
+++ test.sh
@@ -3,6 +3,8 @@ rm -rf ./test_dir
 mkdir ./test_dir
 dd if=/dev/zero of=./test_dir/file_one bs=1024 count=1 
 dd if=/dev/zero of=./test_dir/file_two bs=2048 count=1
+dd if=/dev/zero of=./test_dir/file_three bs=2024 count=2050
+dd if=/dev/zero of=./test_dir/file_four bs=2048 count=1
 ./bin/fstats
 rm -rf ./test_dir
 
